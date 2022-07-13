26d3a68a06d8b45ee7c4bc4fb81840c2408446d3
mengyi <yiii9292@gmail.com>
2022-07-13 17:02:24 -0400

    cart table image fix

--- client/src/views/CartView.vue
+++ client/src/views/CartView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cart-view">
-    <h1>Cart View</h1>
+    <h1>Your Shopping Cart</h1>
     <cart-table></cart-table>
   </div>
 </template>
@@ -16,4 +16,16 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.cart-view > h1 {
+  font-weight: 500;
+  letter-spacing: 0.027em;
+  line-height: 1.33em;
+  padding: 1rem 5rem;
+  border: 1px solid #dedede;
+  border-left: none;
+  border-right: none;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+</style>
