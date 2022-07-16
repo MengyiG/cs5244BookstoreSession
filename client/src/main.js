e88197eb5ca0b83e5e7e2c000e5750c3577a9995
mengyi <yiii9292@gmail.com>
2022-07-16 11:15:51 -0400

    cart update to local storage

--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "@/router";
 import "@/assets/css/global.css";
-import store from "@/store";
+import store, { CART_STORAGE_KEY } from "@/store";
 
 Vue.config.productionTip = false;
 
@@ -11,5 +11,12 @@ new Vue({
   store, //every component could reference the store
   render: function (h) {
     return h(App);
+  },
+  created() {
+    const cartString = localStorage.getItem(CART_STORAGE_KEY);
+    if (cartString) {
+      const shoppingCart = JSON.parse(cartString);
+      this.$store.commit("SET_CART", shoppingCart);
+    }
   },
 }).$mount("#app");
--- client/src/store.js
+++ client/src/store.js
@@ -4,6 +4,8 @@ import ApiService from "@/services/ApiService";
 import { ShoppingCart } from "@/models/ShoppingCart";
 Vue.use(Vuex);
 
+export const CART_STORAGE_KEY = "cart";
+
 export default new Vuex.Store({
   state: {
     categories: [],
@@ -25,12 +27,22 @@ export default new Vuex.Store({
     },
     ADD_TO_CART(state, book) {
       state.cart.addItem(book, 1);
+      localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(this.state.cart));
     },
     // UPDATE_CART(state, item) {
     //   state.cart.update(item.book, item.quality);
     // },
     UPDATE_CART(state, { book, quantity }) {
       state.cart.update(book, quantity);
+      localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(this.state.cart));
+    },
+    SET_CART(state, shoppingCart) {
+      localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(shoppingCart));
+      let newCart = new ShoppingCart();
+      shoppingCart.items.forEach((item) => {
+        newCart.addItem(item.book, item.quantity);
+      });
+      state.cart = newCart;
     },
   },
   actions: {
